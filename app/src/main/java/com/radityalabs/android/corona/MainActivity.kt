cc1e2675cb2221034c18e6c7af5d166bf1732f0b
radityagumay <gumay.raditya@gmail.com>
2020-03-11 21:29:39 +0700

    initialize

---
+++ app/src/main/java/com/radityalabs/android/corona/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.radityalabs.android.corona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.radityalabs.android.corona.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/radityalabs/android/corona/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.radityalabs.android.corona.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
